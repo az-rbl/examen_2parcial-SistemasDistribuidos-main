34453c7e23d9c5bd1fbcbaeae6cfa6a4be860fa4
az-rbl <bl.raul@outlook.com>
2024-04-05 02:15:56 -0600

    funciona flask con db

--- flask-app/app.py
+++ flask-app/app.py
@@ -5,42 +5,57 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:postgres@db/mydatabase'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:postgres@db:5432/mydatabase'
 
 db = SQLAlchemy(app)
 
 class Paquete(db.Model):
     id_paquete = db.Column(db.Integer, primary_key=True)
     estado = db.Column(db.String(50))
+    
 
 
-paquetes = {} #TODO: Cambiar a una base de datos
+#paquetes = {} #TODO: Cambiar a una base de datos
+
+
+@app.route('/')
+def hello():
+    # Create the tables if they don't exist
+    db.create_all()
 
 @app.route('/paquetes', methods=['GET'])
 def get_paquetes():
-    return jsonify(paquetes)
+    paquetes = Paquete.query.all()
+    resultado = []
+    for paquete in paquetes:
+        paquete_datos ={'id_paquete': paquete.id_paquete, 'estado': paquete.estado}
+        resultado.append(paquete_datos)
+    return jsonify(paquete_datos)
 
 @app.route('/paquete', methods=['POST'])
 def registrar_paquete():
-    datos = request.json
-    id_paquete = datos['id']
-    paquetes[id_paquete] = {'estado': 'registrado'}
-    return jsonify({'mensaje': 'Paquete registrado con éxito'}), 201
+    datos = request.get_json()
+    id = datos.get('id')
+    db.session.add(Paquete(id_paquete=id, estado='registrado'))
+    db.session.commit()
+    return jsonify({'mensaje': 'Paquete registrado con éxito'}), 200
 
 @app.route('/paquete/<id>', methods=['GET'])
 def obtener_estado_paquete(id):
-    paquete = paquetes.get(id)
+    paquete = Paquete.query.get(id)
     if paquete:
-        return jsonify({'id': id, 'estado': paquete['estado']})
+        return jsonify({'id': id, 'estado': paquete.estado})
     else:
         return jsonify({'mensaje': 'Paquete no encontrado'}), 404
     
 @app.route('/paquete/<id>', methods=['PUT'])
 def actualizar_estado_paquete(id):
-    datos = request.json
+    datos = request.get_json()
     estado = datos.get('estado')
-    if id in paquetes:
-        paquetes[id]['estado'] = estado
+    paquete = Paquete.query.get(id)
+    if paquete:
+        paquete.estado = estado
+        db.session.commit()
         return jsonify({'mensaje': 'Estado actualizado'})
     else:
         return jsonify({'mensaje': 'Paquete no encontrado'}), 404
